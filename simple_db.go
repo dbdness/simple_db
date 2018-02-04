18fe57b1b4bdacd6dbe7a84e0a80c7920bb1ba6e
Danny Nielsen <danny.nlsn0@gmail.com>
2018-02-04 15:54:00 +0100

    Added CLI handling

--- simple_db.go
+++ simple_db.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	database "./database"
+)
+
 func main() {
+	usercommand := os.Args[1]
+	if usercommand == "" {
+		log.Panic("No arguments given.")
+		return
+	}
+
+	switch usercommand {
+	case "set":
+		database.Put(os.Args[2], os.Args[3])
+		fmt.Println("Key and value successfully stored")
+	case "get":
+		fmt.Println(database.Get(os.Args[2]))
+	}
 
 }
